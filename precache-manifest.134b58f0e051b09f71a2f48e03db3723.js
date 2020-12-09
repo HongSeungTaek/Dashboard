766fc565ae233cbd5e0f68037f467821d19cd3a6
HongSeungTaek <tmdxor66@naver.com>
2020-12-09 23:15:40 +0900

    Updates

---
+++ precache-manifest.134b58f0e051b09f71a2f48e03db3723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac821acb1cf3e1acb2f03aa84b48523b",
+    "url": "/Dashboard/index.html"
+  },
+  {
+    "revision": "551f1de4ef228dea5348",
+    "url": "/Dashboard/static/css/2.7bf98c55.chunk.css"
+  },
+  {
+    "revision": "9a5359262df8d569c8f4",
+    "url": "/Dashboard/static/css/main.526c1a71.chunk.css"
+  },
+  {
+    "revision": "551f1de4ef228dea5348",
+    "url": "/Dashboard/static/js/2.e33acb13.chunk.js"
+  },
+  {
+    "revision": "9a5359262df8d569c8f4",
+    "url": "/Dashboard/static/js/main.de95976d.chunk.js"
+  },
+  {
+    "revision": "d8bc9ec02fd6e05b6a8d",
+    "url": "/Dashboard/static/js/runtime~main.8d25dc92.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/profile/precache-manifest.cbbe67f47d3f479fad9578ac8200f56b.js"
+  "/Dashboard/precache-manifest.134b58f0e051b09f71a2f48e03db3723.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/profile/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
