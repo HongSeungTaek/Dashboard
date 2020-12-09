a606b7f87124c5371b5e1a25beb3ea1cdc7d77ca
HongSeungTaek <tmdxor66@naver.com>
2020-12-09 23:13:17 +0900

    부트스트랩 적용 및 플러그인으로 추가한 라이브러리 삭제

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Dashboard from './dashboard/dashboard';
+
 
 function App() {
   return (
     <div className="App">
+      <Dashboard/>
     </div>
   );
 }
--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import './App.css';
+import './dashboard.css';
 
 function Dashboard() {
   return (
     <div className="db-container">
+      <div className="row">
+        <div className="col-4">
+          <PieChart/>
+        </div>
+        <div className="col-4"></div>
+        <div className="col-4"></div>
+      </div>
     </div>
   );
 }
 
-export default App;
+function PieChart() {
+  return (
+    <div className="card">
+        <div className="card-header">Pie Chart</div>
+        <div className="card-body">차트 나올예정</div>
+    </div>
+  );
+}
+
+function LineChart() {
+  return (
+    <div className="card">
+        <div className="card-header">Line Chart</div>
+        <div className="card-body">차트 나올예정</div>
+    </div>
+  );
+}
+
+export default Dashboard;
