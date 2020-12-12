e1f9b84690a9f51eb2b69b3ccd4fdaa530bd4173
HongSeungTaek <tmdxor66@naver.com>
2020-12-13 02:19:55 +0900

    바 차트 추가 및 소스 정리

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Dashboard.css';
-import { Line } from 'react-chartjs-2';
+import LineChart from '../component/charts/Line';
 import PieChart from '../component/charts/Pie';
+import StackedBar from '../component/charts/Bar';
+
 
 function Dashboard() {
   let props = {
@@ -10,6 +12,9 @@ function Dashboard() {
     },
     line_chart: {
       header: 'Line Chart',
+    },
+    bar_chart: {
+      header: 'Bar Chart',
     }
   }
 
@@ -33,51 +38,17 @@ function Dashboard() {
             </div>
           </div>
         </div>
-        <div className="col-4"></div>
+        <div className="col-4">
+          <div className="card">
+            <div className="card-header">{props.bar_chart.header}</div>
+            <div className="card-body">
+              <StackedBar/>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
-
-var data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
-
-class LineChart extends React.Component {
-
-  render() {
-    return (
-          <Line ref="chart" data={data} />
-    );
-  }
-
-  componentDidMount() {
-    const { datasets } = this.refs.chart.chartInstance.data
-    console.log(datasets[0].data);
-  }
 }
 
 export default Dashboard;
