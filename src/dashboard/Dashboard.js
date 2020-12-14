ab76eea333d62c4eb562b0d23a07fc21b068807a
HongSeungTaek <tmdxor66@naver.com>
2020-12-14 23:41:00 +0900

    도넛차트 기본바 차트 등 추가

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -2,7 +2,10 @@ import React from 'react';
 import './Dashboard.css';
 import LineChart from '../component/charts/Line';
 import PieChart from '../component/charts/Pie';
-import StackedBar from '../component/charts/Bar';
+import StackedBar from '../component/charts/StackedBar';
+import VerticalBar from '../component/charts/VerticalBar';
+import DoughnutChart from '../component/charts/Doughnut';
+import GroupedBar from '../component/charts/GroupedBar';
 
 
 function Dashboard() {
@@ -10,23 +13,40 @@ function Dashboard() {
     pie_chart: {
       header: 'Pie Chart',
     },
+    dounut_pie_chart: {
+      header: 'Doughnut Pie Chart',
+    },
     line_chart: {
       header: 'Line Chart',
     },
-    bar_chart: {
-      header: 'Bar Chart',
+    vertical_bar_chart: {
+      header: 'VerticalBar Chart',
+    },
+    stacked_bar_chart: {
+      header: 'StackedBar Chart',
+    },
+    grouped_bar_chart: {
+      header: 'GroupedBar Chart',
     }
   }
 
 
   return (
     <div className="db-container">
-      <div className="row">
+      <div className="row mb15">
         <div className="col-4">
           <div className="card">
             <div className="card-header">{props.pie_chart.header}</div>
             <div className="card-body">
               <PieChart/>
+            </div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card">
+            <div className="card-header">{props.dounut_pie_chart.header}</div>
+            <div className="card-body">
+              <DoughnutChart/>
             </div>
           </div>
         </div>
@@ -38,11 +58,29 @@ function Dashboard() {
             </div>
           </div>
         </div>
+      </div>
+      <div className="row mb15">
         <div className="col-4">
           <div className="card">
-            <div className="card-header">{props.bar_chart.header}</div>
+            <div className="card-header">{props.vertical_bar_chart.header}</div>
+            <div className="card-body">
+              <VerticalBar/>
+            </div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card">
+            <div className="card-header">{props.stacked_bar_chart.header}</div>
             <div className="card-body">
               <StackedBar/>
+            </div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card">
+            <div className="card-header">{props.grouped_bar_chart.header}</div>
+            <div className="card-body">
+              <GroupedBar/>
             </div>
           </div>
         </div>
